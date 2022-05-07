ee30062850a5b4f33aa6b572595bf4feae84f243
guonan <838093891@qq.com>
2022-05-08 00:45:17 +0800

    add: mathcers

--- test/matcher/matcher.test.js
+++ test/matcher/matcher.test.js
@@ -31,3 +31,75 @@ test('测试undefined', () => {
   expect(a).toBeUndefined() // 匹配成功 (严格匹配)
 })
 
+/**
+ * toBeTruthy匹配器
+ */
+test('测试布尔值', () => {
+  const a = true
+  expect(a).toBeTruthy() // 匹配成功
+})
+
+/**
+ * toBeFalsy匹配器
+ */
+test('测试布尔值', () => {
+  const a = false
+  expect(a).toBeFalsy() // 匹配成功
+})
+
+///////////////////////// 数字相关匹配器 ///////////////////////////////
+/**
+ * toBeCloseTo匹配器【计算浮点数】
+ */
+test('测试计算浮点数', () => {
+  const a = 0.1
+  const b = 0.2
+  expect(a + b).toBeCloseTo(0.3) // 匹配成功
+})
+/**
+ * toBeGreaterThan匹配器
+ */
+/**
+ * toBeGreaterThanOrEqual匹配器
+ */
+/**
+ * toBeLessThan匹配器
+ */
+/**
+ * toBeLessThanOrEqual匹配器
+ */
+/**
+ * toBeNaN匹配器
+ */
+/**
+ * toBeNegativeInfinity匹配器
+ */
+
+///////////////////////// 字符串相关匹配器 ///////////////////////////////
+/**
+ * toMatch匹配器
+ */
+test('测试字符串', () => {
+  const a = 'guo-nan'
+  expect(a).toMatch(/guo/) // 匹配成功
+})
+
+///////////////////////// Array - Set相关匹配器 ///////////////////////////////
+/**
+ * toContain匹配器
+ */
+test('测试数组', () => {
+  const a = [1, 2, 3] // new Set([1, 2, 3])
+  expect(a).toContain(2) // 匹配成功
+})
+
+const throwNewError = () => {
+  throw new Error('测试异常')
+}
+
+/**
+ * 异常匹配器
+ */
+test('测试异常', () => {
+  expect(throwNewError).toThrow()
+})
