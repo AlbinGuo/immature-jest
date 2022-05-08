e145e3b7ec836a34882a9a9be643e03b095f9aa4
guonan <838093891@qq.com>
2022-05-08 11:21:08 +0800

    feat: describe 分组

--- test/hooks/Counter.test.js
+++ test/hooks/Counter.test.js
@@ -1,50 +1,62 @@
 import Counter from './Counter';
 
-let counter = null
+describe('Counter测试',() => {
+  let counter = null
 
-beforeAll(() => {
-  console.log('beforeAll')
-})
+  beforeAll(() => {
+    console.log('beforeAll')
+  })
 
-afterAll(() => {
-  console.log('afterAll')
-})
+  afterAll(() => {
+    console.log('afterAll')
+  })
 
-/**
- * 每次执行测试之前都会执行beforeEach
- */
-beforeEach(() => {
-  console.log('beforeEach')
-  counter = new Counter()
-})
+  /**
+   * 每次执行测试之前都会执行beforeEach
+   */
+  beforeEach(() => {
+    console.log('beforeEach')
+    counter = new Counter()
+  })
 
-/**
- * 每次执行测试之后都会执行afterEach
- */
-afterEach(() => {
-  console.log('afterEach')
-})
+  /**
+   * 每次执行测试之后都会执行afterEach
+   */
+  afterEach(() => {
+    console.log('afterEach')
+  })
 
-test('测试Counter中addOne方法1', () => {
-  console.log('测试Counter中addOne方法1')
-  counter.addOne();
-  expect(counter.number).toBe(1);
-})
+  /**
+   * 分组：测试addOne方法
+   */
+  describe('测试增加相关代码', () => {
+    test('测试Counter中addOne方法1', () => {
+      console.log('测试Counter中addOne方法1')
+      counter.addOne();
+      expect(counter.number).toBe(1);
+    })
+    
+    test('测试Counter中addOne方法2', () => {
+      console.log('测试Counter中addOne方法2')
+      counter.addOne();
+      expect(counter.number).toBe(1);
+    })
+  })
 
-test('测试Counter中addOne方法2', () => {
-  console.log('测试Counter中addOne方法2')
-  counter.addOne();
-  expect(counter.number).toBe(1);
-})
-
-test('测试Counter中minus方法1', () => {
-  console.log('测试Counter中minus方法1')
-  counter.minusOne();
-  expect(counter.number).toBe(-1);
-})
-
-test('测试Counter中minus方法2', () => {
-  console.log('测试Counter中minus方法2')
-  counter.minusOne();
-  expect(counter.number).toBe(-1);
+  /**
+   * 分组：测试minusOne方法
+   */
+  describe('测试减法相关代码', () => {
+    test('测试Counter中minus方法1', () => {
+      console.log('测试Counter中minus方法1')
+      counter.minusOne();
+      expect(counter.number).toBe(-1);
+    })
+    
+    test('测试Counter中minus方法2', () => {
+      console.log('测试Counter中minus方法2')
+      counter.minusOne();
+      expect(counter.number).toBe(-1);
+    })
+  })
 })
